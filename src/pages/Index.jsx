6f687471d350b855cab4f7f745b34f5accb2264d
Orlando <169166413+OrlandoDiaz2003@users.noreply.github.com>
2025-10-10 17:37:56 -0300

    first commit

---
+++ src/pages/Index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+  return (
+    <main className="main-background">
+      <h1 className="title-index">Trending</h1>
+      <p className="p-index">
+        Explore the catalog of upcoming manga releases and discover which new
+        series and volumes are on the way. A guide designed to keep you always
+        up to date in one place.
+      </p>
+      <div id="card_containter" className="index-manga-card"></div>
+    </main>
+  );
+}
